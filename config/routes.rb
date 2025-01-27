0db1da7bca0f3f77e40893a1da0ff8cbe9ce3653
cercil0605 <kuamaichi2223@gmail.com>
2025-01-28 08:25:02 +0900

    change: ZoomControllers and some source

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  post '/zoom_meetings', to: 'meetings#create'
+  # indexのルートを追加する
+  get 'zoom/oauth', to: 'zoom_controllers#oauth'
+  get 'zoom/oauth/callback', to: 'zoom_controllers#oauth_callback'
+  post 'zoom/create_meeting', to: 'zoom_controllers#create_meeting'
 end
