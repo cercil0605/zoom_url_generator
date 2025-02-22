f135e521f5fb7282d92009f84eb3acf6795cb1ea
cercil0605 <67260558+cercil0605@users.noreply.github.com>
2025-02-22 10:50:26 +0000

    fixed: cors problem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,5 @@ Rails.application.config.session_store :cookie_store,
   key: '_zoom_url_generator_session', 
   expire_after: 1.hour,  # 必要に応じてセッションの有効期限を設定
   domain: :all,         # サブドメインでもクッキーを共有する場合
-  same_site: :none    # セッションがクロスサイトで送信されないようにする
+  same_site: :lax,   # こいつが原因だった、許さない
+  secure: Rails.env.production?  # 本番環境でのみセキュアなクッキーを使用
